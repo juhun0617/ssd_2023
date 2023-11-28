00ad810f04995bda05ca36715c7e843ba2e7705e
juhun-park <juhun0617@gmail.com>
2023-11-28 10:45:19 +0900

    update entity

---
+++ src/main/java/org/example/Entity/Deco.java
@@ -0,0 +1,27 @@
+package org.example.Entity;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Deco {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String decoName;
+    private String decoClass;
+    private int decoPrice;
+    private String decoImagePath;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
