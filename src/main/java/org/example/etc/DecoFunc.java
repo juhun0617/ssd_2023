92ecba0c0580417eb7dd7ecd5b9838ddae229739
juhun-park <juhun0617@gmail.com>
2023-12-01 08:45:10 +0900

    add foodFunc()

--- src/main/java/org/example/etc/DecoFunc.java
+++ src/main/java/org/example/etc/DecoFunc.java
@@ -26,13 +26,41 @@ public class DecoFunc {
     }
     public void funcStart(){
         if (Objects.equals(deco.getDecoClass(),"back")){
-            backFunc();
+            backGroundFunc();
+        } else if (Objects.equals(deco.getDecoClass(),"food")){
+            foodFunc();
         }
     }
 
-    private void backFunc(){
+    private void backGroundFunc(){
         character.setBackId(deco.getId());
         characterService.saveCharacter(character);
+    }
+    private void foodFunc(){
+        if (deco.getId() == 13) { //약 먹었을 때
+            character.setHealth(character.getHealth() + 30);
+            characterService.saveCharacter(character);
+        } else if (deco.getId() == 14){ //도넛 먹었을 때
+            character.setHungry(character.getHungry() + 30);
+            characterService.saveCharacter(character);
+        } else if (deco.getId() == 15){ //케이크 먹었을 때
+            character.setHungry(character.getHungry() + 60);
+            characterService.saveCharacter(character);
+        } else if (deco.getId() == 16){ //오렌지 주스 먹었을 때
+            character.setThirst(character.getThirst() + 20);
+            characterService.saveCharacter(character);
+        } else if (deco.getId() == 17){ //커피
+            character.setThirst(character.getThirst() + 30);
+            character.setHealth(character.getHealth() + 10);
+            character.setFun(character.getFun() + 20);
+            characterService.saveCharacter(character);
+        } else if (deco.getId() == 18){ //스무디 (궁극의 음료)
+            character.setThirst(100);
+            character.setHealth(100);
+            character.setFun(100);
+            character.setHungry(100);
+            characterService.saveCharacter(character);
+        }
     }
 
 }
